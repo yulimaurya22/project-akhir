238e7880ad525fc5e02f1cdd282951bcec83057f
yulimaurya22 <yulimaurya@gmail.com>
2023-12-18 19:42:40 +0700

    get data api

--- Screen/home.js
+++ Screen/home.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { SafeAreaView, Text, View, FlatList, ActivityIndicator } from 'react-native'
 
 const Item = ({name}) => {
     <View style ={styles.item}>
         <Text style={styles.title}>{name}</Text>
 
-    </View>
-    
-};
+    </View>};
 const Home = () => {
-    const [isLoading, setLoading] = useState(true);
+
+  //menampung data dari api
     const [data, setData] = useState([]);
+
+    //fungsi untuk mengambil data
+    const getDataFromApiAsync = async () => {
+      try {
+      let response = await fetch('https://makeup-api.herokuapp.com/api/v1/products.json')
+      let json = await response.json()
+      setData(json);
+      }catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  //otomatis dijalankan
+      
+
     return(
       <SafeAreaView style={styles.container}>
         {isLoading  ? (
@@ -25,5 +40,5 @@ const Home = () => {
       </SafeAreaView>
     );
 
-}
+
 export default Home
